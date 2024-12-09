9f5073339b7d37797070849522ab39e00cb0cc73
Freast East <crazyketchup123@gmail.com>
2024-12-09 22:21:03 +0000

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -4,58 +4,46 @@
 */
 
 
-/**
-* Custom blocks
-*/
 //% block="FreastLog" weight=100 color=#ffc2ed icon=""
 namespace FreastLog {
+    export class LogEntry {
+        constructor(
+            public name: string,
+            public dialog: string,
+            public char1: Image[],
+            public char2: Image[],
+            public priority: number
+        ) {}
 
-    /**
-    * TODO: describe your function here
-    * @param n "NAME"
-    * @param d "DIALOG"
-    * @param p "PRIORITY"
-    */
-    //export let name: string = null
-    //export let dialog: string = null
-    //export let char1: Array<Image> = null
-    //export let char2: Array<Image> = null
-    //export let priority: number = null
-
-    //% block
+    }
     
-    export function Log(name:  string, dialog: string, c1: Array<Image>, c2: Array<Image>, p: number): Array<any> 
+    //% block
+    export function Log(name:  string, dialog: string, c1: Array<Image>, c2: Array<Image>, p: number): LogEntry 
     {
-        let s: Array<any> = []
-        s[0] = name;
-        s[1] = dialog;
-        s[2] = c1;
-        s[3] = c2;
-        s[4]  = p;
-        return s;
+        return new LogEntry(name, dialog, c1, c2, p)
     }
     //% block
-    export function Name(dialogue: Array<any>): string{
-        return dialogue[0]
+    export function Name(dialogue: LogEntry): string{
+        return dialogue.name
     }
 
     //% block
-    export function Dialog(dialogue: Array<any>): string {
-        return dialogue[1]
+    export function Dialog(dialogue: LogEntry): string {
+        return dialogue.dialog
     }
 
     //% block
-    export function Char1(dialogue: Array<any>): Array<Image> {
-        return dialogue[2]
+    export function Char1(dialogue: LogEntry): Image[] {
+        return dialogue.char1
     }
 
     //% block
-    export function Char2(dialogue: Array<any>): Array<Image> {
-        return dialogue[3]
+    export function Char2(dialogue: LogEntry): Image[] {
+        return dialogue.char2
     }
 
     //% block
-    export function Priority(dialogue: Array<any>): number {
-        return dialogue[4]
+    export function Priority(dialogue: LogEntry): number {
+        return dialogue.priority
     }
 }
